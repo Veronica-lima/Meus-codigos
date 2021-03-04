68910e7e996410a841753cdf80de4d6ffc8e5312
Veronica-lima <79770802+Veronica-lima@users.noreply.github.com>
2021-03-04 19:11:12 -0300

    Add files via upload

---
+++ Questao1.py
@@ -0,0 +1,14 @@
+'''Converta as seguintes expressões matemáticas para que possam ser calculadas usando
+Python'''
+
+Expresao1 = 10 + (20 * 30)
+print(Expresao1)
+
+Expresao2 = (4 ** 2 ) / 3
+print(Expresao2)
+
+Expresao3 = (9 ** 4 + 2) * (6 - 1)
+print(Expresao3)
+
+Expresao4 = ((9 ** 4 + 2) ** 4) + (10 / 1)
+print(Expresao3)
