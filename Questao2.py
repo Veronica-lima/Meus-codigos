4fcf35ddb33ca6368953747c57d91ea04949efe1
Veronica-lima <79770802+Veronica-lima@users.noreply.github.com>
2021-03-04 19:22:38 -0300

    Add files via upload

---
+++ Questao2.py
@@ -0,0 +1,13 @@
+'''Escreva uma função que receba dois números como argumento e retorne o produto do dobro
+do primeiro pelo triplo do segundo'''
+
+def numeros(num1, num2):
+    return (num1 * 2 *(num2 * 3))
+print(numeros(2,1))
+
+
+
+
+
+
+
